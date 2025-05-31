20ab7aeecf7e6f5b177223e1a80f814c7b019961
Ra-Wh <152503415+Ra-Wh@users.noreply.github.com>
2025-05-31 21:10:54 +0100

    Create ticket form

--- app/forms.py
+++ app/forms.py
@@ -1,13 +1,17 @@
 import sqlalchemy as sa
 
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired, Email, EqualTo
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, SelectField, TextAreaField
+from wtforms.validators import DataRequired, Email, EqualTo, Length, Regexp
 
 class LoginForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired()])
-    password = PasswordField('Password', validators=[DataRequired()])
-    remember_me = BooleanField('Remeber Me')
+    username = StringField('Username', validators=[DataRequired(), Length(max=50)])
+    password = PasswordField('Password', validators=[
+        DataRequired(),
+        Length(min=12, max=64),
+        Regexp(r'^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])', 
+            message="Password must include at least one uppercase letter, one lowercase letter, one number, and one special character.")
+    ])
     submit = SubmitField('Login')
 
 class RegistrationForm(FlaskForm):
@@ -20,3 +24,9 @@ class RegistrationForm(FlaskForm):
     password2 = PasswordField(
         'Repeat Password', validators=[DataRequired(), EqualTo('password')])
     submit = SubmitField('Register')
+
+class CreateTicketForm(FlaskForm):
+    issue_type = SelectField('Issue Type', choices=[(1, 'incident'),(2, 'request'), (3, 'support')], validators=[DataRequired()])
+    priority = SelectField('Priority', choices=[(1, 'Low'), (2, 'Medium'), (3, 'High'), (4, 'Critical')], validators=[DataRequired()])
+    summary = TextAreaField('Summary', validators=[DataRequired()])
+    details = TextAreaField('Details', validators=[DataRequired()])
