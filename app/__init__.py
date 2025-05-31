4f8b44713c63712f67a236055afc8a41ce80e8fd
Rachel.W <152503415+Ra-Wh@users.noreply.github.com>
2025-05-31 13:10:51 +0100

    database creation (#18)

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,19 @@
 from flask import Flask
+from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_login import LoginManager
 
 app = Flask(__name__)
+app.config.from_object(Config)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+login = LoginManager(app)
+login.init_app(app)
+login.login_view = 'login'
+login.login_message = None
 
-from app import routes
+if __name__ == '__main__':
+    app.run(debug=True)
+
+from app import routes, models
