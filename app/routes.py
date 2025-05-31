021a4feab27c1538c15bd331c592e7069534affc
Ra-Wh <152503415+Ra-Wh@users.noreply.github.com>
2025-05-31 15:24:17 +0100

    Login page

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,32 @@
-from app import app
+from flask import render_template, flash, redirect, url_for, request
+from app.forms import LoginForm
+from app import app, db
+from flask_login import current_user, logout_user, login_required, login_user
+import sqlalchemy as sa
+from app.models import login_details, user_accounts
+from urllib.parse import urlsplit
+from flask import session
+
 @app.route('/')
 @app.route('/index')
+@login_required
 def index():
     return "Support Hub"
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = db.session.scalar(
+            sa.select(login_details).where(login_details.username == form.username.data))
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)
+        next_page = request.args.get('next')
+        if not next_page or urlsplit(next_page).netloc != '':
+            next_page = url_for('index')
+        return redirect(next_page)
+    return render_template('login.html', title='Sign in', form=form)
